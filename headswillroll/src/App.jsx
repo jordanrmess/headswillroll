4b2daa08df8f09a4cb800949ecb77ea7ef730987
Jordan Mess <jordanmess@jordans-mbp.mynetworksettings.com>
2025-11-05 13:40:33 -0500

    add timeout

--- headswillroll/src/App.jsx
+++ headswillroll/src/App.jsx
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [imageSrc, setImageSrc] = useState('heads/c.png');
+  const [timeoutId, setTimeoutId] = useState(null);
 
   useEffect(() => {
     const handleMouseMove = (event) => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+
       const viewportCenterX = window.innerWidth / 2;
       const viewportCenterY = window.innerHeight / 2;
-
       const deltaX = event.clientX - viewportCenterX;
       const deltaY = event.clientY - viewportCenterY;
-
       const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
 
       if (angle >= -22.5 && angle < 22.5) {
@@ -30,6 +33,11 @@ const App = () => {
       } else if (angle >= -67.5 && angle < -22.5) {
         setImageSrc('heads/ur.png');
       }
+
+      const id = setTimeout(() => {
+        setImageSrc('heads/c.png');
+      }, 2000); // 2000ms delay
+      setTimeoutId(id);
     };
 
     window.addEventListener('mousemove', handleMouseMove);
@@ -37,13 +45,15 @@ const App = () => {
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     };
-  }, []);
+  }, [timeoutId]);
 
   return (
-    <div className="flex ]justify-center items-center min-h-screen">
-      <img src={imageSrc} className="w-24 h-24 object-contain m-auto"
- />
-    </div>
+    <div className="flex justify-center items-center h-screen">
+      <img
+        src={imageSrc}
+        alt="Direction"
+        className="w-16 h-16 object-contain"
+      />    </div>
   );
 };
 
