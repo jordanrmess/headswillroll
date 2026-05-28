48ab56395ce921a88b13ce442fc8b0e890a2345c
Jordan Mess <jordanmess@jordans-mbp.lan>
2026-05-28 14:14:27 -0400

    added email

--- src/App.jsx
+++ src/App.jsx
@@ -108,6 +108,18 @@ const App = () => {
           onClick={() => flipLightSwitch(!lightSwitch)}
         />
       </div>
+      <p
+        className={`absolute bottom-4 left-1/2 z-10 -translate-x-1/2 text-center text-sm sm:bottom-6 sm:text-base ${
+          lightSwitch ? "text-slate-800" : "text-slate-200"
+        }`}
+      >
+        <a
+          href="mailto:jordanrmess@gmail.com"
+          className="underline underline-offset-2 hover:opacity-80"
+        >
+          jordanrmess@gmail.com
+        </a>
+      </p>
     </div>
   );
 };
