c2b47d030f8ff4e472d921807974f9178b646000
Jordan Mess <jordanmess@Jordans-MacBook-Pro.local>
2026-07-27 14:41:29 -0400

    added peaking message

--- src/Envelope.jsx
+++ src/Envelope.jsx
@@ -1,34 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import publicUrl from "./utils/publicUrl";
 import Modal from "./Modal";
 
-const Envelope = ({ envelopeOpen, lightsOn, onToggle }) => (
-  <div>
-    <div className="absolute top-0 left-0 z-10 p-8 pt-44">
-      {" "}
-      <img
-        src={publicUrl(
-          envelopeOpen
-            ? lightsOn
-              ? "envelope/envelope_open.svg"
-              : "envelope/envelope_open_inverse.svg"
-            : lightsOn
-              ? "envelope/envelope_closed.svg"
-              : "envelope/envelope_closed_inverse.svg",
-        )}
-        className="relative w-35 h-35 cursor-pointer transition-transform duration-300 hover:scale-110"
-        onClick={onToggle}
-        draggable={false}
-      />
+const Envelope = ({ envelopeOpen, lightsOn, onToggle }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const showOpenEnvelope = envelopeOpen || isHovered;
+
+  return (
+    <div>
+      <div className="absolute top-0 left-0 z-10 p-8 pt-44">
+        <img
+          src={publicUrl(
+            showOpenEnvelope
+              ? lightsOn
+                ? "envelope/envelope_open.svg"
+                : "envelope/envelope_open_inverse.svg"
+              : lightsOn
+                ? "envelope/envelope_closed.svg"
+                : "envelope/envelope_closed_inverse.svg",
+          )}
+          className="relative w-35 h-35 cursor-pointer transition-transform duration-300 hover:scale-110"
+          onClick={onToggle}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          draggable={false}
+        />
+      </div>
+      {envelopeOpen && (
+        <Modal
+          message="jordan r mess at  gmail dot com"
+          lightsOn={lightsOn}
+          onClose={onToggle}
+        />
+      )}
     </div>
-    {envelopeOpen && (
-      <Modal
-        message="jordan r mess at  gmail dot com"
-        lightsOn={lightsOn}
-        onClose={onToggle}
-      />
-    )}
-  </div>
-);
+  );
+};
 
 export default Envelope;
