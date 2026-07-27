22aaaa1b85d234249c2f257a23a676f3ab9ebd8a
Jordan Mess <jordanmess@Jordans-MacBook-Pro.local>
2026-07-27 12:51:44 -0400

    made switch bigger

--- src/LightSwitchButton.jsx
+++ src/LightSwitchButton.jsx
@@ -9,7 +9,7 @@ const LightSwitchButton = ({ lightsOn, onToggle }) => (
           ? "switches/on_switch_transparent.svg"
           : "switches/off_switch_transparent.svg",
       )}
-      className="w-20 h-20 cursor-pointer"
+      className="w-35 h-35 cursor-pointer"
       onClick={onToggle}
       alt="Toggle lights"
     />
